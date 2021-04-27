cb8dcdb813dde96962aec1fcd6f1b636d05be44b
Kanguk Lee <rkddnrdl97@naver.com>
2021-04-27 13:21:41 +0900

    ListView, Spinner, WebView

---
+++ ListViewAdapter/app/src/main/java/com/example/listviewadapter/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.listviewadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] names = {"LEE", "CHOI", "KIM", "JEONG", "RHO"};
+        ListView listView = (ListView)findViewById(R.id.listview);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
+    }
+}
